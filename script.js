f7417d9f3ac36cde8f78bd0142867c7f5010c4cf
Reynard Z. Torculas <97606356+reyntors@users.noreply.github.com>
2023-06-14 12:17:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+function openNews() {
+    var overlay = document.getElementById("newsOverlay");
+    overlay.style.display = "flex";
+}
+
+function closeNews() {
+    var overlay = document.getElementById("newsOverlay");
+    overlay.style.display = "none";
+  }
+
+function openEvent() {
+    var overlay = document.getElementById("eventOverlay");
+    overlay.style.display = "flex";
+}
+function closeEvent() {
+    var overlay = document.getElementById("eventOverlay");
+    overlay.style.display = "none";
+  }
+
+function openAdvisory() {
+    var overlay = document.getElementById("advisoryOverlay");
+    overlay.style.display = "flex";
+}
+function closeAdvisory() {
+    var overlay = document.getElementById("advisoryOverlay");
+    overlay.style.display = "none";
+  }
+
+function openUpdates() {
+    var overlay = document.getElementById("UpdatesOverlay");
+    overlay.style.display = "flex";
+}
+function closeUpdates() {
+    var overlay = document.getElementById("UpdatesOverlay");
+    overlay.style.display = "none";
+  }
+
+function openRequest() {
+    var overlay = document.getElementById("RequestOverlay");
+    overlay.style.display = "flex";
+}
+function closeRequest() {
+    var overlay = document.getElementById("RequestOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openAppointment() {
+    var overlay = document.getElementById("AppointmentOverlay");
+    overlay.style.display = "flex";
+}
+function closeAppointment() {
+    var overlay = document.getElementById("AppointmentOverlay");
+    overlay.style.display = "none";
+  }
