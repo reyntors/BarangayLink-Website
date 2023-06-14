87c220a5a2210eed03e121a52996ce684cccd1c1
Reynard Z. Torculas <97606356+reyntors@users.noreply.github.com>
2023-06-15 01:58:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,49 @@ function closeAppointment() {
     var overlay = document.getElementById("AppointmentOverlay");
     overlay.style.display = "none";
   }
+
+  function openMessage() {
+    var overlay = document.getElementById("MessageOverlay");
+    overlay.style.display = "flex";
+}
+function closeMessage() {
+    var overlay = document.getElementById("MessageOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openAnnouncement() {
+    var overlay = document.getElementById("AnnouncementOverlay");
+    overlay.style.display = "flex";
+}
+function closeAnnouncement() {
+    var overlay = document.getElementById("AnnouncementOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openForums() {
+    var overlay = document.getElementById("ForumsOverlay");
+    overlay.style.display = "flex";
+}
+function closeForums() {
+    var overlay = document.getElementById("ForumsOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openDiscussions() {
+    var overlay = document.getElementById("DiscussionsOverlay");
+    overlay.style.display = "flex";
+}
+function closeDiscussions() {
+    var overlay = document.getElementById("DiscussionsOverlay");
+    overlay.style.display = "none";
+  }
+
+  function openFeedbacks() {
+    var overlay = document.getElementById("FeedbacksOverlay");
+    overlay.style.display = "flex";
+}
+function closeFeedbacks() {
+    var overlay = document.getElementById("FeedbacksOverlay");
+    overlay.style.display = "none";
+  }
+
